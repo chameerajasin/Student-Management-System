25cdbdf8ded054d63a7939eb1df0c580d87e7832
chameerajasin <chameerajasin@gmail.com>
2026-03-19 21:42:20 +0530

    feat: add new StudentNotFoundException contructor with username

--- services/student-service/src/main/java/com/chameera/student/student/exception/StudentNotFoundException.java
+++ services/student-service/src/main/java/com/chameera/student/student/exception/StudentNotFoundException.java
@@ -7,16 +7,10 @@ import lombok.Getter;
 public class StudentNotFoundException extends RuntimeException {
     private final int statusCode;
 
-    //these were created to get the idea. we can create custom constructors if needed for different use cases
-//    public StudentNotFoundException(String message) {
-//        super(message);
-//        statusCode = 404;
-//    }
-//
-//    public StudentNotFoundException(String message, Throwable cause) {
-//        super(message, cause);
-//        statusCode = 404;
-//    }
+    public StudentNotFoundException(String username) {
+        super("Student not found for username: " + username);
+        statusCode = 404;
+    }
 
     public StudentNotFoundException(Long studentId) {
         super("Student with ID " + studentId + " not found");
